6c4ade024b13210d8cca3e1d6760b803ab37022d
Brian Hwang <brianhwang29@gmail.com>
2023-11-28 09:45:05 +0900

    mvvm binding

---
+++ WeatherApp_MVVM/ViewModel/WeatherVM.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp_MVVM.Model;
+
+namespace WeatherApp_MVVM.ViewModel
+{
+    class WeatherVM : INotifyPropertyChanged
+    {
+
+        private string query;
+
+        public string Query
+        {
+            get => query;
+            set { 
+                query = value; 
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get => currentConditions; 
+            set 
+            { 
+                currentConditions = value;
+                OnPropertyChanged("CurrentConditions");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get =>selectedCity;
+            set
+            {  
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        // this is just to check if the binding is working correctly
+        public WeatherVM()
+        {
+            SelectedCity = new City
+            {
+                LocalizedName = "New York"
+            };
+
+            CurrentConditions = new CurrentConditions
+            {
+                WeatherText = "Partly Cloudy",
+                Temperature = new Temperature
+                {
+                    Metric = new Units
+                    {
+                        Value = 21
+                    }
+                }
+            };
+        }
+
+        //Interface
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
